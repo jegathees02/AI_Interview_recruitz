48f53cb5acd440d8423f08b9d6f0cc9d5398037c
Sanjeevi-CS <sanjeevisakthivel2004@gmail.com>
2024-01-25 13:44:39 +0530

    Charts Skeleton

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -4,7 +4,91 @@ import { useParams } from "react-router-dom";
 import NavBar from "../components/navbar";
 import ApexChart from "../Charts/linechart";
 import ApexCharts from "apexcharts";
+import ReactApexChart from "react-apexcharts";
 const Dashboard = () => {
+    const generateData = (count, range) => {
+        const data = [];
+        for (let i = 0; i < count; i++) {
+          data.push({
+            x: i + 1,
+            y: Math.floor(Math.random() * (range.max - range.min + 1)) + range.min,
+          });
+        }
+        return data;
+      };
+  const [options, setOptions] = useState({
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    title: {
+      text: "Product Trends by Month",
+      align: "left",
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"],
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+      ],
+    },
+  });
+
+  const [series, setSeries] = useState([
+    {
+      name: "Desktops",
+      data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+    },
+  ]);
+
+  const [chartData, setChartData] = useState({
+    series: [
+      { name: "Metric1", data: generateData(18, { min: 0, max: 90 }) },
+      { name: "Metric2", data: generateData(18, { min: 0, max: 90 }) },
+      { name: "Metric3", data: generateData(18, { min: 0, max: 90 }) },
+      { name: "Metric4", data: generateData(18, { min: 0, max: 90 }) },
+      { name: "Metric5", data: generateData(18, { min: 0, max: 90 }) },
+      { name: "Metric6", data: generateData(18, { min: 0, max: 90 }) },
+      { name: "Metric7", data: generateData(18, { min: 0, max: 90 }) },
+      { name: "Metric8", data: generateData(18, { min: 0, max: 90 }) },
+      { name: "Metric9", data: generateData(18, { min: 0, max: 90 }) },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "heatmap",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      colors: ["#008FFB"],
+      title: {
+        text: "HeatMap Chart (Single color)",
+      },
+    },
+  });
+
   return (
     <div>
       <>
@@ -42,8 +126,8 @@ const Dashboard = () => {
           </div>
         </div>
 
-        <div className="flex lg:flex-row sm:flex-col flex-col w-[calc(100%-72px)] mx-auto justify-between">
-          <div className="lg:w-[calc(40%-4px)] sm:w-full h-[506px] bg-black mt-[8px] rounded-lg border border-borders">
+        <div className="flex lg:flex-row sm:flex-col flex-col w-[calc(100%-72px)] mx-auto gap-4 justify-between">
+          <div className="lg:w-[358px] sm:w-full h-[506px] bg-black mt-[8px] rounded-lg border border-borders">
             <div className="text-[22px] font-bold mt-[40px] text-white ml-[50px]">
               Community Stats
             </div>
@@ -57,15 +141,137 @@ const Dashboard = () => {
               <span className="text-text_2  text-white">Reputation:</span> 789
             </div>
           </div>
-          <div className="lg:w-[calc(60%-4px)] sm:w-full sm: h-[506px] h-[506px] bg-white mt-[8px] rounded-lg border border-borders relative">
-            <div className="flex sm:flex-row flex-col justify-between">
+          <div className="lg:w-full sm:w-full sm: h-[506px] h-[506px] bg-white mt-[8px] rounded-lg border border-borders relative">
+            <div className="flex sm:flex-row flex-col">
               <div className="flex flex-col relative mr-[50px] mt-[40px] w-[200px] sm:w-[280px] ml-[50px] sm:ml-0">
-                <div className="text-black mt-4 ml-12" id="app"></div>
+                <div id="chart">
+                  <ReactApexChart
+                    options={options}
+                    series={series}
+                    type="line"
+                    height={450}
+                    width={450}
+                  />
+                </div>
               </div>
-              <div className="flex flex-col relative mr-[50px] mt-[40px] w-[200px] sm:w-[280px] ml-[50px] sm:ml-0">
-                <div className="text-black mt-4 ml-12">Hi2</div>
+              <div className="flex flex-col relative mr-[20px] mt-[40px] w-[200px] sm:w-[280px]  sm:ml-0">
+                <div className="text-black mt-4 ml-[17.5rem]">
+                  <div id="chart">
+                    <ReactApexChart
+                      options={options}
+                      series={series}
+                      type="area"
+                      height={450}
+                      width={450}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+        {/* Table */}
+        <div className="w-[calc(100%-72px)] h-full sm:h-full bg-gray-100 mx-auto mt-[8px] rounded-lg border border-borders">
+          <table class="w-full caption-bottom text-sm">
+            <thead class="[&amp;_tr]:border-b">
+              <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 max-w-[150px]">
+                  Name
+                </th>
+                <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  Status
+                </th>
+                <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  Inventory
+                </th>
+                <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
+                  Vendor
+                </th>
+              </tr>
+            </thead>
+            <tbody class="[&amp;_tr:last-child]:border-0">
+              <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
+                  Glimmer Lamps
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  In Production
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+                  500 in stock
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  Luminance Creations
+                </td>
+              </tr>
+              <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
+                  Aqua Filters
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  Available for Order
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+                  750 in stock
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  HydraClean Solutions
+                </td>
+              </tr>
+              <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
+                  Eco Planters
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  Backordered
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+                  300 in stock
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  GreenGrowth Designers
+                </td>
+              </tr>
+              <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
+                  Zest Juicers
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  Newly Launched
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+                  1000 in stock
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  FreshTech Appliances
+                </td>
+              </tr>
+              <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
+                  Flexi Wearables
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  Selling Fast
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+                  200 in stock
+                </td>
+                <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
+                  Vitality Gear Co.
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        {/* Heat Map */}
+        <div className="w-[calc(100%-72px)] h-full sm:h-full bg-black-100 mx-auto mt-[8px] rounded-lg border border-borders">
+          <div id="chart">
+            <ReactApexChart
+              options={chartData.options}
+              series={chartData.series}
+              type="heatmap"
+              height={350}
+            />
           </div>
         </div>
       </>
